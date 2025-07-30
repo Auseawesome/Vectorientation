8b8e22afeb715869d18dea7499ab213b4c6a4c6e
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2025-07-30 10:46:40 +0100

    Remove unnecessary imports

--- src/main/java/dev/perxenic/vectorientation/Vectorientation.java
+++ src/main/java/dev/perxenic/vectorientation/Vectorientation.java
@@ -1,15 +1,10 @@
 package dev.perxenic.vectorientation;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
-import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
-import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
