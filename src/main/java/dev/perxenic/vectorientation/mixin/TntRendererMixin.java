f5d770f92c60bf7419ecc3e2ef0bdd8efc09738c
Auseawesome <70506988+Auseawesome@users.noreply.github.com>
2025-07-30 20:02:37 +0100

    Simplify TNT blacklist check

--- src/main/java/dev/perxenic/vectorientation/mixin/TntRendererMixin.java
+++ src/main/java/dev/perxenic/vectorientation/mixin/TntRendererMixin.java
@@ -6,6 +6,7 @@ import dev.perxenic.vectorientation.Vectorientation;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.TntRenderer;
 import net.minecraft.world.entity.item.PrimedTnt;
+import net.minecraft.world.level.block.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,7 +22,7 @@ public class TntRendererMixin {
             method = "render(Lnet/minecraft/world/entity/item/PrimedTnt;FFLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V"
     )
     public void addRotation(PrimedTnt entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
-        if (Config.blacklist.contains(entity.getBlockState().getBlock())) return;
+        if (Config.blacklist.contains(Blocks.TNT)) return;
         Vectorientation.addRotation(entity, poseStack);
     }
 }
